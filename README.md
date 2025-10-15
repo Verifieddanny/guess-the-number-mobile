6648357a74d74a7bda2389c0af9df7590a6d9974
Verifieddanny <dannyclassic56@gmail.com>
2025-10-15 23:35:42 +0100

    feat: initial push

---
+++ README.md
@@ -0,0 +1,5 @@
+# GUESS THE NUMBER MOBILE APP
+
+<!-- ![Demo GIF](/public/demo.gif) -->
+
+<img src="./public/demo.gif" alt="Demo GIF" width="600" />
---
+++ components/ui/Card.js
@@ -0,0 +1,28 @@
+import { View, StyleSheet, Dimensions } from "react-native";
+import Colors from "../../constants/colors";
+
+function Card({ children }) {
+  return <View style={styles.card}>{children}</View>;
+}
+
+export default Card;
+
+const deviceWidth = Dimensions.get('window').width;
+
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: deviceWidth < 380 ? 18 : 36,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    elevation: 4, // for Android shadow
+    shadowColor: "black", // for iOS shadow
+    shadowOffset: { width: 0, height: 2 }, // for iOS shadow
+    shadowRadius: 6, // for iOS shadow
+    shadowOpacity: 0.25, // for iOS shadow
+  },
+});
